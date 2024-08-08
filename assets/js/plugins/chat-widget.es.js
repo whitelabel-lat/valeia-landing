8c147647e29cc4b7ba58a211a243712597788704
Daniel Lopez <daniel.lopez@fsfb.org.co>
2024-08-08 16:18:07 -0500

    add new widget

--- assets/js/plugins/chat-widget.es.js
+++ assets/js/plugins/chat-widget.es.js
@@ -1,13 +1,34 @@
-var o = Object.defineProperty;
-var a = (e, t, n) => t in e ? o(e, t, { enumerable: !0, configurable: !0, writable: !0, value: n }) : e[t] = n;
-var i = (e, t, n) => a(e, typeof t != "symbol" ? t + "" : t, n);
+var a = Object.defineProperty;
+var o = (i, t, s) => t in i ? a(i, t, { enumerable: !0, configurable: !0, writable: !0, value: s }) : i[t] = s;
+var e = (i, t, s) => o(i, typeof t != "symbol" ? t + "" : t, s);
 class h {
+  // Imagen para el estado abierto
   constructor() {
-    i(this, "isOpen", !1);
-    i(this, "container");
-    i(this, "iframe");
-    i(this, "button");
-    this.container = document.createElement("div"), this.iframe = document.createElement("iframe"), this.button = document.createElement("button"), this.initializeElements(), this.attachEventListeners(), this.render();
+    e(this, "isOpen", !1);
+    e(this, "container");
+    e(this, "iframe");
+    e(this, "button");
+    e(this, "floatingText");
+    e(this, "openImgSrc", "https://cdn-icons-png.flaticon.com/512/13888/13888994.png");
+    // Imagen para el estado cerrado
+    e(this, "closeImgSrc", "https://cdn-icons-png.flaticon.com/512/8695/8695043.png");
+    this.container = document.createElement("div"), this.iframe = document.createElement("iframe"), this.button = document.createElement("button"), this.floatingText = document.createElement("div"), this.initializeElements(), this.attachEventListeners(), this.initializeStyles(), this.render();
+  }
+  initializeStyles() {
+    this.iframe.style.width = "400px", this.iframe.style.height = "600px", this.iframe.style.right = "30px", this.iframe.style.bottom = "100px", this.floatingText.innerText = "¡Hola! ¿como puedo ayudarte?", this.floatingText.style.position = "absolute", this.floatingText.style.width = "300px", this.floatingText.style.textAlign = "center", this.floatingText.style.top = "10px", this.floatingText.style.right = "80px", this.floatingText.style.backgroundColor = "rgba(110,36,255, 0.8)", this.floatingText.style.color = "white", this.floatingText.style.padding = "5px", this.floatingText.style.borderRadius = "5px", this.floatingText.style.animation = "wave 2s ease-in-out infinite", this.floatingText.style.display = "inline-block";
+    const t = document.styleSheets[0];
+    t.insertRule(`
+            @keyframes wave {
+                0%, 100% {
+                    transform: translateY(0);
+                    transform: translateX(0);
+                }
+                50% {
+                    transform: translateY(10px);
+                    transform: translateX(10px);
+                }
+            }
+        `, t.cssRules.length);
   }
   initializeElements() {
     this.container.style.cssText = `
@@ -30,31 +51,30 @@ class h {
             opacity: 0;
             transition: all 0.3s ease-in-out;
             overflow: hidden;
-        `, this.button.textContent = "M", this.button.style.cssText = `
+        `, this.button.style.cssText = `
             display: flex;
             justify-content: center;
             align-items: center;
             height: 55px;
             width: 55px;
-            border-radius: 50%;
-            background-color: #25D366;
-            color: white;
-            font-size: 30px;
+            // border-radius: 50%;
+            // background-color: #25D366;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
             border: none;
             cursor: pointer;
             transition: all 0.2s ease-in-out;
-        `;
+            padding: 0;
+        `, this.updateButtonImage();
   }
   attachEventListeners() {
     this.button.addEventListener("click", this.handleClick.bind(this)), this.button.addEventListener("mouseover", () => {
-      this.button.style.backgroundColor = "white", this.button.style.color = "#25D366";
+      this.button.style.backgroundColor = "white";
     }), this.button.addEventListener("mouseout", () => {
-      this.button.style.backgroundColor = "#25D366", this.button.style.color = "white";
+      this.button.style.backgroundColor = "white";
     }), window.addEventListener("resize", this.updateIframeSize.bind(this));
   }
   handleClick() {
-    this.isOpen = !this.isOpen, this.updateIframeVisibility(), this.updateButtonText();
+    this.isOpen = !this.isOpen, this.updateIframeVisibility(), this.updateButtonImage();
   }
   updateIframeVisibility() {
     this.isOpen ? (this.updateIframeSize(), setTimeout(() => {
@@ -63,22 +83,23 @@ class h {
       this.iframe.style.width = "0", this.iframe.style.height = "0";
     }, 300));
   }
-  updateButtonText() {
-    this.button.textContent = this.isOpen ? "X" : "M";
+  updateButtonImage() {
+    const t = this.isOpen ? this.closeImgSrc : this.openImgSrc;
+    this.button.style.backgroundImage = `url('${t}')`, this.button.style.backgroundSize = "cover", this.button.style.backgroundPosition = "center", this.button.style.backgroundRepeat = "no-repeat";
   }
   updateIframeSize() {
     window.innerWidth <= 1200 ? (this.iframe.style.width = "90vw", this.iframe.style.height = "80vh", this.iframe.style.maxWidth = "400px", this.iframe.style.maxHeight = "600px", this.iframe.style.right = "5vw") : (this.iframe.style.width = "400px", this.iframe.style.height = "600px", this.iframe.style.right = "30px"), this.iframe.style.bottom = "100px";
   }
   render() {
-    this.container.appendChild(this.iframe), this.container.appendChild(this.button);
+    this.container.appendChild(this.iframe), this.container.appendChild(this.button), this.container.appendChild(this.floatingText);
   }
   mount() {
     document.body.appendChild(this.container);
   }
 }
-function s() {
+function n() {
   new h().mount();
 }
 window.ChatWidgetConfig = window.ChatWidgetConfig || {};
-window.ChatWidgetConfig.autoInit !== !1 && (document.readyState === "loading" ? document.addEventListener("DOMContentLoaded", s) : s());
-window.initChatWidget = s;
+window.ChatWidgetConfig.autoInit !== !1 && (document.readyState === "loading" ? document.addEventListener("DOMContentLoaded", n) : n());
+window.initChatWidget = n;
